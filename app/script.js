cacc2ae51d007552943caab7d4195393edfd4082
Kenji Crosland <kenji.crosland@gmail.com>
2015-10-11 12:32:20 -0700

    Completed 1-9 still need to add bonus features

--- app/script.js
+++ app/script.js
@@ -24,13 +24,31 @@ $(function() {
     e.preventDefault();
 
     var adjective = $("input[name=adjective]").val();
+    var noun = $("input[name=noun]").val();
+    var verb = $("input[name=verb").val();
     var adjPost;
+    var nounPost;
+    var verbPost;
 
     if (adjective) {
       adjPost = {word: adjective};
       $.post("adjective", adjPost, function(response) {
         var adjectiveRes = response.msg;
         $("#adjectiveRes").text(adjectiveRes);
+      });
+    }
+    if (noun) {
+      nounPost = {word: noun};
+      $.post("noun", nounPost, function(response){
+        var nounRes = response.msg;
+        $("#nounRes").text(nounRes);
+      });
+    }
+    if (verb) {
+      verbPost = {word: verb};
+      $.post("verb", verbPost, function(response){
+        var verbRes = response.msg;
+        $("#verbRes").text(verbRes);
       });
     }
 
